6b3f79eaf8771f9d31a8334f82a238f591514dd8
Ryan Johnston <github@shopandlearn.net>
2014-04-14 11:40:53 -0400

    Initial Commit
    Add PHP Docblock.

---
+++ form.php
@@ -0,0 +1,18 @@
+<?php
+/** Simple upload form demonstrating a smart honeypot.
+ * Processes the form to stop spam bots. Renames the normal fields on this page.
+ * Then, creates a honeypot with one of the names of the original fields.
+ * The Honeypot is removed with JavaScript.
+ *
+ * @author Ryan Johnston
+ * @copyright 2014 Ryan Johnston
+ * @license MIT
+ * @link https://github.com/freak3dot/smart-honeypot
+ * @version 0.1.0-beta
+ * @todo Add the form
+ */
+
+
+
+
+?>
